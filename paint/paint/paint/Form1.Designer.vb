b94a162de87f377fff3d5f6db5c9c00e4bf535c1
danielsilver7 <31133990+danielsilver7@users.noreply.github.com>
2017-08-18 10:03:21 -0400

    paint

---
+++ paint/paint/paint/Form1.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(900, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(964, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.SystemColors.Window
+        Me.Button9.ForeColor = System.Drawing.SystemColors.Window
+        Me.Button9.Location = New System.Drawing.Point(924, 60)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(58, 53)
+        Me.Button9.TabIndex = 12
+        Me.Button9.Text = "Button9"
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(903, 119)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(105, 23)
+        Me.Button10.TabIndex = 13
+        Me.Button10.Text = "choose a color"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1244, 119)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.paint.My.Resources.Resources.white
+        Me.ClientSize = New System.Drawing.Size(1399, 456)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents Button1 As Button
+End Class
---
+++ paint/paint/paint/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+    Dim mybrush As New SolidBrush(Color.Black)
+    Dim xPos As Integer
+    Dim yPos As Integer
+    Dim PxPos As Integer
+    Dim PyPos As Integer
+    Dim radius As Integer = 3
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        Dim draw As System.Drawing.Graphics
+        draw = Me.CreateGraphics
+        Label1.Text = xPos
+        Label2.Text = yPos
+        PxPos = xPos
+        PyPos = yPos
+
+        xPos = Me.MousePosition.X - Me.Location.X - 15
+        yPos = Me.MousePosition.Y - Me.Location.Y - 40
+
+
+
+        If mouse = True Then
+            draw.DrawLine(New Pen(mybrush, radius), PxPos, PyPos, xPos, yPos)
+        End If
+    End Sub
+    Dim mouse As Boolean = False
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        mouse = True
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp
+        mouse = False
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        mybrush.Color = Color.Red
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        mybrush.Color = Color.Orange
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs)
+        mybrush.Color = Color.Yellow
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
+        mybrush.Color = Color.Green
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs)
+        mybrush.Color = Color.Blue
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs)
+        mybrush.Color = Color.Purple
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        mybrush.Color = Color.White
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        ColorDialog1.ShowDialog()
+        mybrush.Color = ColorDialog1.Color
+    End Sub
+
+    Private Sub Form1_MouseWheel(sender As Object, e As MouseEventArgs) Handles Me.MouseWheel
+        radius += 1
+    End Sub
+End Class
